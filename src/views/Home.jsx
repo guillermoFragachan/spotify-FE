2a7abea4c0a2d300d79b74e6b8f56c7b9b04630a
guillermoFragachan <3945636+guillermoFragachan@users.noreply.github.com>
2021-12-06 13:10:46 +0100

    fix home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const Home = () => {
 
+  const navigate = useNavigate();
+
   const [songs, setSongs] = useState([]);
+  const [recentSongs, setRecentSongs] = useState([]);
 
   const getSongs = (artist) => {
     fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" +artist)
     .then((response) => response.json())
       .then((data) => {
         setSongs(data.data);
-        console.log(data.data, 'try')
+        // console.log(data.data, 'try')
       })
 
   }
 
+  const getRecentSongs = (artist) => {
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q="+artist)
+    .then((response) => response.json())
+    .then((data) => {
+      setRecentSongs(data.data);
+      console.log(data.data, 'try')
+    })
+  }
   useEffect(() => {
     getSongs("eminem")
+    getRecentSongs("metallica")
+    
   },[])
 
 
@@ -30,8 +44,10 @@ const Home = () => {
                   <div class="row py-2" id="good-mor">
 
                     {
-                      songs.map((song, index) => (
-                         <div className="col-6, col-md-4 ,col-lg-3">
+                      songs.slice(0, 6).map((song, index) => (
+                         <div className="col-6, col-md-4 ,col-lg-3" onClick={()=>{
+                            navigate(`/artist/${song.artist.id}`)
+                         }}>
                             <div class="card-top p-0 mb-2">
                           <div class="row no-gutters">
                             <div class="col-md-4">
@@ -65,16 +81,39 @@ const Home = () => {
                 </div>
               </section>
             </div>
-            <div class="main-content-album">
+            
+            <div class="main-content-album2">
               <div>
                 <div class="padding-content">
                   <section id="second-section">
                     <div class="container" >
-                      <div class="d-flex justify-content-between mt-3">
+                      <div class="d-flex justify-content-between mt-5">
                         <h4 style={{width: "bold"}}>Recently played</h4>
                         <p><span class="text-muted"> SEE All</span></p>
                       </div>
                       <div class="row py-1 d-flex" id="recently-played">
+                      {
+                        recentSongs.slice(0, 12).map((song, index) => {
+                           
+                          return <div className="col-2" onClick={()=>{
+                            navigate(`/album/${song.album.id}`)
+                          }}>
+                          <div class="card" style={{marginBottom:"25px"}}>
+                          <img
+                            src={song.album.cover_medium}
+                            class="card-img-top img-fluid"
+                          />
+                          <div class="card-body">
+                            <h5 class="card-title dotted">{song.album.title}</h5>
+                          </div>
+                      </div>
+                      </div>
+                        })
+                      }
+
+                      
+
+
 
                       </div>
                     </div>
