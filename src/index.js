6beb8e219f2872d7d88b3d34ef17bce2f7446c69
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-12-06 16:46:46 +0100

    fixed  indentation of the code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import configureStore from './store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import configureStore from "./store";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import "./styles/SideBar.css"
-import "./styles/HomePage.css"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./styles/SideBar.css";
+import "./styles/HomePage.css";
 
 ReactDOM.render(
-  <Provider store={configureStore }>
-
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
-,
-  document.getElementById('root')
+  <Provider store={configureStore}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
 
 reportWebVitals();
